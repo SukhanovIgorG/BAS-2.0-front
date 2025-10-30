82b81d71c03492c8c98c750c6b4c88444f2ba74a
SukhanovIgorG <sukhanovgarik@yandex.ru>
2025-10-30 09:38:44 +0300

    fix: link to item

--- src/features/spaces/spaces.page.tsx
+++ src/features/spaces/spaces.page.tsx
@@ -3,28 +3,15 @@ import { Link } from 'react-router-dom';
 import { ROUTES } from '@/shared/model/routes';
 
 function SpacesPage() {
+  const spaceId = '1'; // пример ID
+
   return (
     <div>
       <h2>Список пространств</h2>
       <ul>
         <li>
-          <Link
-            to={{
-              pathname: `${ROUTES.SPACES}`,
-            }}
-            title="Items"
-          >
+          <Link to={ROUTES.SPACE.replace(':spaceId', spaceId)} title="Items">
             item 1
-          </Link>
-        </li>
-        <li>
-          <Link to={ROUTES.SPACES} title="Items">
-            item 2
-          </Link>
-        </li>
-        <li>
-          <Link to={ROUTES.SPACES} title="Items">
-            item 3
           </Link>
         </li>
       </ul>
